a2fa8eca4be78947c205fcd18cd70221e003f059
Paula Sierra <PaulaCode01@gmail.com>
2022-12-11 18:07:40 -0500

    Manage account container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { CreatePasswordComponent } from './components/create-password/create-password.component';
 import { EmailSendComponent } from './components/email-send/email-send.component';
 import { LoginComponent } from './components/login/login.component';
+import { ManageAccountComponent } from './components/manage-account/manage-account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreatePasswordComponent,
     EmailSendComponent,
-    LoginComponent
+    LoginComponent,
+    ManageAccountComponent
   ],
   imports: [
     BrowserModule
