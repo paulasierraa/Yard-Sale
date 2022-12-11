77b9bdb1ec8c544d2d79bc68ce38ba9731e746c1
Paula Sierra <PaulaCode01@gmail.com>
2022-12-11 11:31:42 -0500

    create-password component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CreatePasswordComponent } from './components/create-password/create-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreatePasswordComponent
   ],
   imports: [
     BrowserModule
